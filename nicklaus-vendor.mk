5e8e06ef8821c6a969ba5d6e3e251786102a165e
zac6ix <nanaquameallday@gmail.com>
2017-10-23 20:43:49 +0000

    Update and rename woods-vendor.mk to nicklaus-vendor.mk

---
+++ nicklaus-vendor.mk
@@ -0,0 +1,19 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/nicklaus/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/nicklaus/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/nicklaus/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/nicklaus/priv-app,system/priv-app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/nicklaus/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/nicklaus/vendor/bin,system/vendor/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/nicklaus/vendor/etc,system/vendor/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/nicklaus/vendor/firmware,system/vendor/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/nicklaus/vendor/lib,system/vendor/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/nicklaus/vendor/thh,system/vendor/thh)
